7646e3c3f59d932396701647503bd14818e30ce2
Eunice Igbinedion <eunice.igbinedion@yahoo.com>
2022-09-05 13:37:15 +0200

    Updating Chat Component

--- allproject/client/src/components/ChatContainer.js
+++ allproject/client/src/components/ChatContainer.js
@@ -1,33 +1,66 @@
-import ChatHeader from './ChatHeader'
-import MatchesDisplay from './MatchesDisplay'
-import ChatDisplay from './ChatDisplay'
-import { useState } from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import ChatDisplay from "./ChatDisplay";
+import ChatHeader from "./ChatHeader";
+import MatchesDisplay from "./MatchesDisplay";
 
-const ChatContainer = ({ user }) => {
-  const [clickedUser, setClickedUser] = useState()
+const ChatContainer = () => {
+  const [clickedUser, setClickedUser] = useState();
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const [user, setUser] = useState(null);
+  const userId = cookies.UserId;
+  // @desc      get all users & get current user
+  const getUser = async () => {
+    try {
+      // const response = await axios.get('http://localhost:8000/user', {
+      //     params: {userId}
+      // })
+      //   setUser(user.data)
+
+      // const response = await axios.get(`http://localhost:8000/users`)
+      // setUsers(response.data)
+
+      const activeUser = await axios.get(
+        `http://localhost:8000/users/${userId}`
+      );
+
+      setUser(activeUser.data);
+    } catch (error) {
+      // console.error(error.message)
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      getUser();
+    };
+  }, []);
+
   return (
-    <div className='chat-container'>
+    <div className="chat-container">
       <ChatHeader user={user} />
 
       <div>
-        <button className='option' onClick={() => setClickedUser(null)}>
+        <button className="option" onClick={() => setClickedUser(null)}>
           Matches
         </button>
-        <button className='option' disabled={!clickedUser}>
+        <button className="option" disabled={!clickedUser}>
           Chat
         </button>
       </div>
 
       {!clickedUser && (
         <MatchesDisplay
-          matches={user&&user.matches}
+          matches={user && user.matches}
           setClickedUser={setClickedUser}
         />
       )}
 
       {clickedUser && <ChatDisplay user={user} clickedUser={clickedUser} />}
     </div>
-  )
-}
+  );
+};
 
-export default ChatContainer
+export default ChatContainer;

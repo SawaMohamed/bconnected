cbcdaacc1f2fc05db059538279ffeee346e28098
Bashar Alwarad <b@gmail.com>
2022-09-07 10:16:56 +0200

    merging main

--- allproject/client/src/components/Nav.js
+++ allproject/client/src/components/Nav.js
@@ -1,41 +1,30 @@
 import Small from '../images/small.png'
 import Logo from '../images/BConnected-logo.png'
 
-
 const Nav = ({ authToken, setShowModal, showModal, setIsSignUp }) => {
-
   // @desc remove form for login
   const handleClick = () => {
-      setShowModal(true)
-      setIsSignUp(false)
+    setShowModal(true)
+    setIsSignUp(false)
   }
 
   return (
     <nav>
       <div className='logo-container'>
-        
-        <img
-          className='logo'
-
-          src={Logo}
-
-
-          alt='logo'
-        />
-      <div className='title'>
-      <h1 className='primary-title'></h1>
-      <h2 className='second-title'></h2>
-      </div>
+        <img className='logo' src={Logo} alt='logo' />
+        <div className='title'>
+          <h1 className='primary-title'></h1>
+          <h2 className='second-title'></h2>
+        </div>
       </div>
 
-      {  (!authToken&&
-
+      {!authToken && (
         <button
           className='nav-button'
           onClick={handleClick}
           disabled={showModal}
         >
-           Log in
+          Log in
         </button>
       )}
     </nav>
